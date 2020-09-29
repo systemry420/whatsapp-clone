bc567560d113226fc8324cc87fd484167fbeddfe
hsn <systemry420gmail.com>
2020-09-29 18:28:20 +0300

    implement sidebar search bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Sidebar.css' ;
 import { Avatar, IconButton } from "@material-ui/core"
+import { SearchOutlined } from "@material-ui/icons"
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -24,8 +25,11 @@ function Sidebar() {
                 </div>
             </div>
 
-            <div className="side__search">
-
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start a new chat" type="text" />
+                </div>
             </div>
             <div className="sidebar__chats">
 
