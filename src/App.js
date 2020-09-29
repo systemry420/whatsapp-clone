f359225a8816a50011a5f3ce98cfdfa5f95d5b07
hsn <systemry420gmail.com>
2020-09-29 15:13:56 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__body">
+          {/* Sidebar */}
+          {/* Chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
